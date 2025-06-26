f8842b014b3aa44d592345a9e29a91657efe4156
AlexanderKa <x182838@gmail.com>
2025-06-26 15:20:46 +0200

    Task Выбранные митапы

--- 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
+++ 02-basics-2/50-selected-meetup/SelectedMeetupApp.js
@@ -1,78 +1,90 @@
-import { defineComponent } from 'vue'
-// import { getMeetup } from './meetupsService.ts'
+import { defineComponent, ref, computed, watch } from 'vue'
+import { getMeetup } from './meetupsService.ts'
 
 export default defineComponent({
   name: 'SelectedMeetupApp',
 
-  setup() {},
+  setup() {
+    const meetupCount  = ref(5)
+    const currentMeetupId = ref(1)
+    const currentMeetupTitle = ref('')
+
+    const isButtonPreviousDisabled = computed(() => currentMeetupId.value === 1)
+    const isButtonNextDisabled = computed(() => currentMeetupId.value === meetupCount.value)
+
+    const showPreviousSlide = () => currentMeetupId.value > 1 && --currentMeetupId.value
+    const showNextSlide = () => currentMeetupId.value < meetupCount.value && ++currentMeetupId.value
+
+    const getCurrentMeetupTitle = async (id) => {
+      const meetup = await getMeetup(id)
+      currentMeetupTitle.value = meetup.title
+    }
+
+    watch(currentMeetupId, () => {
+      getCurrentMeetupTitle(currentMeetupId.value)
+    },{immediate: true})
+
+    return {
+      meetupCount,
+      currentMeetupId,
+      isButtonPreviousDisabled,
+      isButtonNextDisabled,
+      showPreviousSlide,
+      showNextSlide,
+      currentMeetupTitle,
+    }
+  },
 
   template: `
     <div class="meetup-selector">
       <div class="meetup-selector__control">
-        <button class="button button--secondary" type="button" disabled>Предыдущий</button>
+        <button 
+            @click="showPreviousSlide" 
+            class="button button--secondary" 
+            type="button" 
+            :disabled="isButtonPreviousDisabled"
+        >
+          Предыдущий
+        </button>
 
         <div class="radio-group" role="radiogroup">
-          <div class="radio-group__button">
+          <div 
+              v-for="item in meetupCount" 
+              :key="item" 
+              class="radio-group__button"
+          >
             <input
-              id="meetup-id-1"
+              :id="'meetup-id-' + item"
               class="radio-group__input"
               type="radio"
               name="meetupId"
-              value="1"
+              :value="item"
+              v-model="currentMeetupId"
             />
-            <label for="meetup-id-1" class="radio-group__label">1</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-2"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="2"
-            />
-            <label for="meetup-id-2" class="radio-group__label">2</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-3"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="3"
-            />
-            <label for="meetup-id-3" class="radio-group__label">3</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-4"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="4"
-            />
-            <label for="meetup-id-4" class="radio-group__label">4</label>
-          </div>
-          <div class="radio-group__button">
-            <input
-              id="meetup-id-5"
-              class="radio-group__input"
-              type="radio"
-              name="meetupId"
-              value="5"
-            />
-            <label for="meetup-id-5" class="radio-group__label">5</label>
+            <label 
+                :for="'meetup-id-' + item" 
+                class="radio-group__label"
+            >
+              {{ item }}
+            </label>
           </div>
         </div>
 
-        <button class="button button--secondary" type="button">Следующий</button>
+        <button 
+            @click="showNextSlide" 
+            class="button button--secondary" 
+            type="button" 
+            :disabled="isButtonNextDisabled"
+        >
+          Следующий
+        </button>
       </div>
 
       <div class="meetup-selector__cover">
         <div class="meetup-cover">
-          <h1 class="meetup-cover__title">Some Meetup Title</h1>
+          <h1 class="meetup-cover__title">{{ currentMeetupTitle }}</h1>
         </div>
       </div>
-
     </div>
   `,
 })
